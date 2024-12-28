8356f1979c092389ec56ad3507f8d4a1ea3dc854
Go955633 <sanjayyadav215766@gmail.com>
2024-12-28 15:53:49 +0530

    Update Sigma.java
    remove line

--- src/main/java/com/demo/Sigma.java
+++ src/main/java/com/demo/Sigma.java
@@ -3,12 +3,7 @@ package com.demo;
 public class Sigma {
 
 	public static void main(String[] args) {
-		System.out.println(" Working on Github");
-
-		
-		
-		
-			
+		System.out.println(" Working on Github");		
 		}
 	public void m11() {
 		System.out.println("method 11");
