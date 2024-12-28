ebb10182309185daf8d7b13352d9305a9660f802
Sanjay <sanjayyadav215766@gmail.com>
2024-12-28 15:18:34 +0530

    this is my first commit

---
+++ src/main/java/com/demo/Sigma.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class Sigma {
+
+	public static void main(String[] args) {
+		System.out.println(" Working on Github");
+
+		
+		
+		
+			
+		}
+	public void m11() {
+		System.out.println("method 11");
+	}
+	}
+
+
